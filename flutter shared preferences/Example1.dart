a02f8bd4c250bc5c4375dab1527dda006d5653b3
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-10-13 14:58:01 +0530

    Create Example1.dart

---
+++ flutter shared preferences/Example1.dart
@@ -0,0 +1,78 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String data = "hello";
+  TextEditingController textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      data = (prefs.getString("data") ?? "hello");
+      textController.text = data; // Update the input field text
+    });
+  }
+
+  Future<void> setdata(String val) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      data = val;
+      prefs.setString("data", val);
+    });
+  }
+
+  void handleButtonPress() {
+    String inputData = textController.text;
+    setdata(inputData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SharedPreferences Example"),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("hello $data"),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.text,
+                  controller: textController,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: handleButtonPress,
+                child: const Text("Update Data"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
