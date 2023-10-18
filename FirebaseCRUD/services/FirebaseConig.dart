194e53996518b27d93786a11f4bf83d1286cf595
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-10-18 13:17:54 +0530

    Create FirebaseConig.dart

---
+++ FirebaseCRUD/services/FirebaseConig.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+dynamic firebaseRealtimeVariable() {
+  return (const FirebaseOptions(
+      apiKey: "AIzaSyAKBf7u7LAnKRDjFD641-HY6RkbmwIjSpw",
+      authDomain: "kkpucha-d51e2.firebaseapp.com",
+      databaseURL:
+          "https://kkpucha-d51e2-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "kkpucha-d51e2",
+      storageBucket: "kkpucha-d51e2.appspot.com",
+      messagingSenderId: "319369231892",
+      appId: "1:319369231892:web:5024d58486c190a4577101"));
+}
