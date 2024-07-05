7a7c9c0f0c4a460b4042dfd98bce35c4ff87d338
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2024-07-05 21:11:09 +0530

    Update FirebaseConig.dart

--- FirebaseCRUD/services/FirebaseConig.dart
+++ FirebaseCRUD/services/FirebaseConig.dart
@@ -2,12 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 
 dynamic firebaseRealtimeVariable() {
   return (const FirebaseOptions(
-      apiKey: "AIzaSyAKBf7u7LAnKRDjFD641-HY6RkbmwIjSpw",
-      authDomain: "kkpucha-d51e2.firebaseapp.com",
+      apiKey: "xyz",
+      authDomain: "xyz",
       databaseURL:
-          "https://kkpucha-d51e2-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "kkpucha-d51e2",
-      storageBucket: "kkpucha-d51e2.appspot.com",
-      messagingSenderId: "319369231892",
-      appId: "1:319369231892:web:5024d58486c190a4577101"));
+          "xyz",
+      projectId: "xyz",
+      storageBucket: "xyz",
+      messagingSenderId: "xyz",
+      appId: "xyz"));
 }
